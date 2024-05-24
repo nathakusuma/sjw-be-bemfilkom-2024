7057cca30df2e50cf136414b458b88ce9e770664
nathakusuma <nathakusuma07@gmail.com>
2024-05-24 21:32:30 +0700

    refactor: change database credentials env format

--- internal/pkg/database/postgresql/postgresql.go
+++ internal/pkg/database/postgresql/postgresql.go
@@ -1,7 +1,6 @@
 package postgresql
 
 import (
-	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -9,16 +8,7 @@ import (
 )
 
 func Connect() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_DSN")), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
