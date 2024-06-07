00f91e93a5d61f27eb665db189a8c4276da4240c
nathakusuma <nathakusuma07@gmail.com>
2024-06-08 02:40:57 +0700

    ``fix``: redact error data if it's an internal server error

--- internal/pkg/response/api_response.go
+++ internal/pkg/response/api_response.go
@@ -1,6 +1,9 @@
 package response
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 type ApiResponse struct {
 	Status  int
@@ -17,6 +20,12 @@ func NewApiResponse(status int, message string, data any) ApiResponse {
 }
 
 func (r ApiResponse) Send(ctx *gin.Context) {
+	if r.Status >= 500 {
+		if gin.Mode() == gin.ReleaseMode {
+			r.Data = gin.H{}
+		}
+		log.Printf("ERROR %d (%s): %v\n", r.Status, r.Message, r.Data)
+	}
 	ctx.JSON(r.Status, gin.H{
 		"message": r.Message,
 		"data":    r.Data,
