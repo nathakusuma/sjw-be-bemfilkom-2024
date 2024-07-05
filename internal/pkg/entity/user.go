585e52ae29a70257c22316ba6088ec77dffcd155
nathakusuma <nathakusuma07@gmail.com>
2024-07-05 21:29:29 +0700

    ``feat``: add program studi to users table

--- internal/pkg/entity/user.go
+++ internal/pkg/entity/user.go
@@ -1,8 +1,9 @@
 package entity
 
 type User struct {
-	Nim      string `gorm:"primaryKey; type:varchar(15); not null; unique"`
-	Email    string `gorm:"type:varchar(320); not null; unique"`
-	FullName string `gorm:"type:varchar(255); not null"`
-	Role     string `gorm:"type:varchar(255); not null; default:'user'"`
+	Nim          string `gorm:"primaryKey; type:varchar(15); not null; unique"`
+	Email        string `gorm:"type:varchar(320); not null; unique"`
+	FullName     string `gorm:"type:varchar(255); not null"`
+	ProgramStudi string `gorm:"type:varchar(255); not null"`
+	Role         string `gorm:"type:varchar(255); not null; default:'user'"`
 }
